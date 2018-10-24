e8fd648c7b2592690e3cb19d58881e9c36ff6f6c
v86861062 <v86861062@gmail.com>
2018-10-24 15:57:44 +0800

    Updates

---
+++ precache-manifest.e9c146e70e640b77252df7d6fff7c6ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad5dc8ced7a11fe8830",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.1ad5dc8c.js"
+  },
+  {
+    "revision": "f00ad0297ab4f00fe15b",
+    "url": "/fcc-pomodoro-clock/static/js/main.f00ad029.chunk.js"
+  },
+  {
+    "revision": "76a8c4ed033fb0b1ed8a",
+    "url": "/fcc-pomodoro-clock/static/js/1.76a8c4ed.chunk.js"
+  },
+  {
+    "revision": "f00ad0297ab4f00fe15b",
+    "url": "/fcc-pomodoro-clock/static/css/main.0fb59d63.chunk.css"
+  },
+  {
+    "revision": "f11db334a83805de43dacac81ccbaaa5",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
